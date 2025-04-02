6386c10398834cb09f57827009fae6674fbf6a0e
codehashira23 <divyanshjaiswal314@gmail.com>
2025-04-02 23:00:25 +0530

    commit

--- iiitv-icd/src/components/Navbar.jsx
+++ iiitv-icd/src/components/Navbar.jsx
@@ -13,14 +13,18 @@ import {
   navigationMenuTriggerStyle
 } from '@/components/ui/navigation-menu';
 import { cn } from '@/lib/utils';
+import { PlusGrid, PlusGridItem, PlusGridRow } from '@/components/eldoraui/plusgrid';
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
+import { Bars2Icon } from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <header className="w-full">
+    <Disclosure as="header" className="w-full">
       {/* Top bar */}
       <div className="bg-gray-100">
         <div className="container mx-auto py-2 px-4 flex justify-between items-center">
-          <div>
+          <div className="pl-8">
             <Link href="https://iiitvadodara.ac.in/index.php" target="_blank" className="text-sm font-semibold text-yellow-600 hover:text-yellow-700">
               Gandhinagar Campus
             </Link>
@@ -40,42 +44,49 @@ const Navbar = () => {
       </div>
 
       {/* Logo and name */}
-      <div className="bg-white border-b">
-        <div className="container mx-auto py-4 px-4 flex flex-col md:flex-row items-center justify-between">
-          <div className="flex items-center">
-            <Link href="/">
-              <div className="flex items-center">
-                <div className="w-16 h-16 relative mr-3">
-                  <Image 
-                    src="http://diu.iiitvadodara.ac.in/img/Logo5.png" 
-                    alt="IIIT Vadodara Logo" 
-                    width={64} 
-                    height={64}
-                  />
+      <PlusGrid>
+        <PlusGridRow className="relative flex justify-between items-center">
+          <div className="relative flex gap-6 pl-8">
+            <PlusGridItem className="py-3">
+              <Link href="/">
+                <div className="flex items-center">
+                  <div className="w-20 h-20 relative mr-4">
+                    <Image 
+                      src="http://diu.iiitvadodara.ac.in/img/Logo5.png" 
+                      alt="IIIT Vadodara Logo" 
+                      width={80} 
+                      height={80}
+                    />
+                  </div>
+                  <div>
+                    <h1 className="text-2xl font-bold text-[#800000]">IIIT Vadodara-International Campus Diu</h1>
+                    <p className="text-sm text-gray-600">(Satellite campus of IIIT Vadodara)</p>
+                  </div>
                 </div>
-                <div>
-                  <h1 className="text-xl font-bold text-[#800000]">IIIT Vadodara-International Campus Diu</h1>
-                  <p className="text-sm text-gray-600">(Satellite campus of IIIT Vadodara)</p>
-                </div>
-              </div>
-            </Link>
+              </Link>
+            </PlusGridItem>
           </div>
-        </div>
-      </div>
+          <DisclosureButton
+            className="flex size-12 items-center justify-center self-center rounded-lg data-[hover]:bg-black/5 lg:hidden"
+            aria-label="Open main menu"
+          >
+            <Bars2Icon className="size-6" />
+          </DisclosureButton>
+        </PlusGridRow>
+      </PlusGrid>
 
       {/* Main navigation using Radix UI NavigationMenu */}
       <div className="bg-[#800000] text-white relative z-40">
         <div className="container mx-auto px-4">
           <div className="nav-container relative">
-            <NavigationMenu className="mx-auto max-w-none w-full justify-start" viewport={false}>
-              <NavigationMenuList className="flex gap-0 text-white">
-                
+            <NavigationMenu className="mx-auto max-w-none w-full justify-center" viewport={false}>
+              <NavigationMenuList className="flex gap-0 text-white justify-center">
                 {/* Institute dropdown */}
                 <NavigationMenuItem className="relative">
                   <NavigationMenuTrigger className="font-medium text-white bg-[#800000] hover:bg-[#5d0000] focus:bg-[#5d0000] rounded-none h-auto py-3 px-4 data-[state=open]:bg-[#5d0000]">
                     Institute
                   </NavigationMenuTrigger>
-                  <NavigationMenuContent className="bg-white shadow-md rounded-b-md border-0 origin-top-left absolute left-0 z-50">
+                  <NavigationMenuContent className="bg-white shadow-md rounded-b-md border-0 origin-top-center absolute left-1/2 -translate-x-1/2 z-50">
                     <ul className="grid w-[200px] gap-0">
                       <li>
                         <Link href="/about" legacyBehavior passHref>
@@ -142,7 +153,7 @@ const Navbar = () => {
                   <NavigationMenuTrigger className="font-medium text-white bg-[#800000] hover:bg-[#5d0000] focus:bg-[#5d0000] rounded-none h-auto py-3 px-4 data-[state=open]:bg-[#5d0000]">
                     Academics
                   </NavigationMenuTrigger>
-                  <NavigationMenuContent className="bg-white shadow-md rounded-b-md border-0 origin-top-left absolute left-0 z-50">
+                  <NavigationMenuContent className="bg-white shadow-md rounded-b-md border-0 origin-top-center absolute left-1/2 -translate-x-1/2 z-50">
                     <ul className="grid w-[220px] gap-0">
                       <li>
                         <Link href="/btech" legacyBehavior passHref>
@@ -200,16 +211,16 @@ const Navbar = () => {
                           </NavigationMenuLink>
                         </Link>
                       </li>
-                      
                     </ul>
                   </NavigationMenuContent>
                 </NavigationMenuItem>
 
+                {/* Admission dropdown */}
                 <NavigationMenuItem className="relative">
                   <NavigationMenuTrigger className="font-medium text-white bg-[#800000] hover:bg-[#5d0000] focus:bg-[#5d0000] rounded-none h-auto py-3 px-4 data-[state=open]:bg-[#5d0000]">
                     Admission
                   </NavigationMenuTrigger>
-                  <NavigationMenuContent className="bg-white shadow-md rounded-b-md border-0 origin-top-left absolute left-0 z-50">
+                  <NavigationMenuContent className="bg-white shadow-md rounded-b-md border-0 origin-top-center absolute left-1/2 -translate-x-1/2 z-50">
                     <ul className="grid w-[200px] gap-0">
                       <li>
                         <Link href="#" legacyBehavior passHref>
@@ -264,14 +275,12 @@ const Navbar = () => {
                   </NavigationMenuContent>
                 </NavigationMenuItem>
 
-
-
                 {/* People dropdown */}
                 <NavigationMenuItem className="relative">
                   <NavigationMenuTrigger className="font-medium text-white bg-[#800000] hover:bg-[#5d0000] focus:bg-[#5d0000] rounded-none h-auto py-3 px-4 data-[state=open]:bg-[#5d0000]">
                     People
                   </NavigationMenuTrigger>
-                  <NavigationMenuContent className="bg-white shadow-md rounded-b-md border-0 origin-top-left absolute left-0 z-50">
+                  <NavigationMenuContent className="bg-white shadow-md rounded-b-md border-0 origin-top-center absolute left-1/2 -translate-x-1/2 z-50">
                     <ul className="grid w-[200px] gap-0">
                       <li>
                         <Link href="/faculty" legacyBehavior passHref>
@@ -355,14 +364,26 @@ const Navbar = () => {
             /* Override Radix UI's default positioning */
             .nav-container [data-state="open"] > [data-radix-popper-content-wrapper] {
               transform: translate3d(0, 0, 0) !important;
-              left: 0 !important;
+              left: 50% !important;
+              transform: translateX(-50%) !important;
               top: 100% !important;
               margin-top: 0 !important;
             }
           `}</style>
         </div>
       </div>
-    </header>
+
+      {/* Mobile Navigation Panel */}
+      <DisclosurePanel className="lg:hidden">
+        <div className="flex flex-col gap-6 py-4">
+          {/* Add mobile navigation items here */}
+        </div>
+        <div className="absolute left-1/2 w-screen -translate-x-1/2">
+          <div className="absolute inset-x-0 top-0 border-t border-black/5 dark:border-white/5" />
+          <div className="absolute inset-x-0 top-2 border-t border-black/5 dark:border-white/5" />
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
   );
 };
 
